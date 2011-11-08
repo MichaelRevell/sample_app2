34795993509429a5d95255810473663b41bfb38d
Michael <mars8082686@gmail.com>
2011-11-08 18:47:28 +0900

    Rerolled everything

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-SampleApp::Application.config.secret_token = 'a9db8272f064021f1bcee91e123478b825c2337d0f60f342fd96fa3729fb5feabe23f0d10d17ff9e86b5804d8911948085dfc236fbdfd24739f65560d539429d'
+SampleApp::Application.config.secret_token = '1ebf311bc26fb43efb80717312938260d2c6433a502ec1f71b749ab2b5b6dbb5f2ad213ee0d8bde6ed08ab926acce5a83a4926c73f91c04cfe475a0044477905'
